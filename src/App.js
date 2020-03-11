23509b2ac1455a2675df4b2c19c99080091a6937
Ben Parisot <me@benparisot.com>
2020-03-11 11:32:08 -0700

    tweak styling on h1 and stanza height

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Stanza from './Stanza';
 function App() {
   return (
     <div className="App">
+      <h1 className="title">This is just to say</h1>
       {stanzas.map((stanza, i) => (
       <Stanza key={i}>
         <div className="stanza">
